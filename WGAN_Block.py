99003f281e6fcb01601b0bc1116b58cbf33e238f
TIGERC123 <2462924814@qq.com>
2020-03-02 10:28:53 +0800

    layer normalization

--- WGAN_Block.py
+++ WGAN_Block.py
@@ -5,7 +5,7 @@ from tensorflow.keras import layers
 class generator_Input(tf.keras.Model):
   def __init__(self, shape, noise_dim):
     super(generator_Input, self).__init__()
-    self.dense = layers.Dense(shape[0] * shape[1] * shape[2], use_bias=False)
+    self.dense = layers.Dense(shape[0] * shape[1] * shape[2], use_bias=False, kernel_initializer=RandomNormal(stddev=0.02))
     self.reshape = layers.Reshape([shape[0], shape[1], shape[2]])
     self.bn = layers.BatchNormalization(momentum=0.9)
     self.relu = tf.keras.layers.ReLU()
@@ -19,7 +19,7 @@ class generator_Input(tf.keras.Model):
 class generator_Middle(tf.keras.Model):
   def __init__(self, filters, strides, padding):
       super(generator_Middle, self).__init__()
-      self.conv = layers.Conv2DTranspose(filters, kernel_size=4, strides=strides, padding=padding, use_bias=False)
+      self.conv = layers.Conv2DTranspose(filters, kernel_size=4, strides=strides, padding=padding, use_bias=False, kernel_initializer=RandomNormal(stddev=0.02))
       self.bn = layers.BatchNormalization(momentum=0.9)
       self.relu = tf.keras.layers.ReLU()
   def call(self, x):
@@ -31,7 +31,7 @@ class generator_Middle(tf.keras.Model):
 class generator_Output(tf.keras.Model):
   def __init__(self, image_depth, strides, padding):
     super(generator_Output, self).__init__()
-    self.conv = layers.Conv2DTranspose(image_depth, kernel_size=4, strides=strides, padding=padding, use_bias=False)
+    self.conv = layers.Conv2DTranspose(image_depth, kernel_size=4, strides=strides, padding=padding, use_bias=False, kernel_initializer=RandomNormal(stddev=0.02))
     self.actv = layers.Activation(activation='tanh')
   def call(self, x):
     x = self.conv(x)
@@ -41,7 +41,7 @@ class generator_Output(tf.keras.Model):
 class discriminator_Input(tf.keras.Model):
   def __init__(self, filters, strides):
     super(discriminator_Input, self).__init__()
-    self.conv = keras.layers.Conv2D(filters, kernel_size=4, strides=strides, padding="same")
+    self.conv = keras.layers.Conv2D(filters, kernel_size=4, strides=strides, padding="same", kernel_initializer=RandomNormal(stddev=0.02))
     self.leakyRelu = keras.layers.LeakyReLU(alpha=0.2)
     # self.dropout = keras.layers.Dropout(0.3)
 
@@ -54,14 +54,14 @@ class discriminator_Input(tf.keras.Model):
 class discriminator_Middle(tf.keras.Model):
   def __init__(self, filters, strides, padding):
       super(discriminator_Middle, self).__init__()
-      self.conv = tf.keras.layers.Conv2D(filters, kernel_size=4, strides=strides, padding=padding)
-      self.bn = tf.keras.layers.BatchNormalization(momentum=0.9)
+      self.conv = tf.keras.layers.Conv2D(filters, kernel_size=4, strides=strides, padding=padding, kernel_initializer=RandomNormal(stddev=0.02))
+      self.ln = tf.keras.layers.LayerNormalization(momentum=0.9)
       self.leakyRelu = tf.keras.layers.LeakyReLU(alpha=0.2)
       # self.dropout = tf.keras.layers.Dropout(0.3)
 
   def call(self, x):
       x = self.conv(x)
-      x = self.bn(x)
+      x = self.ln(x)
       x = self.leakyRelu(x)
       # x = self.dropout(x)
       return x
@@ -69,7 +69,7 @@ class discriminator_Middle(tf.keras.Model):
 class discriminator_Output_channel(tf.keras.Model):
   def __init__(self, filters, strides, with_activation):
       super(discriminator_Output_channel, self).__init__()
-      self.conv = tf.keras.layers.Conv2D(filters, kernel_size=4, strides=strides, padding="same")
+      self.conv = tf.keras.layers.Conv2D(filters, kernel_size=4, strides=strides, padding="same", kernel_initializer=RandomNormal(stddev=0.02))
       self.flatten = tf.keras.layers.Flatten(name='output')
       if with_activation:
         self.dense = tf.keras.layers.Dense(units=1, activation='sigmoid', name='output')
@@ -87,7 +87,7 @@ class discriminator_Output(tf.keras.Model):
       super(discriminator_Output, self).__init__()
       self.flatten = tf.keras.layers.Flatten()
       if with_activation:
-        self.dense = tf.keras.layers.Dense(units=1, activation='sigmoid', name='output')
+        self.dense = tf.keras.layers.Dense(units=1, activation='sigmoid', name='output', kernel_initializer=RandomNormal(stddev=0.02))
       else:
         self.dense = tf.keras.layers.Dense(1, name='output')
 
